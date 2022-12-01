edaa0d6708668f857cc117bd0bf33179ec094457
denosaurabh <saurabh.fullstackdev@outlook.com>
2022-12-01 15:11:10 +0530

    Added SnarkJS min + circuit data in production

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -9,6 +9,7 @@ import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { Global } from "@emotion/react";
+import Script from "next/script";
 
 const { chains, provider } = configureChains(
   [
@@ -48,12 +49,15 @@ const theme = extendTheme({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider chains={chains}>
-          <ChakraProvider theme={theme}>
-            <Global
-              styles={`
+    <>
+      <Script src="https://cdn.jsdelivr.net/npm/snarkjs@0.5.0/build/snarkjs.min.js" />
+
+      <ChakraProvider theme={theme}>
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider chains={chains}>
+            <ChakraProvider theme={theme}>
+              <Global
+                styles={`
                 /* latin */
                 @font-face {
                   font-family: 'Recoleta';
@@ -64,12 +68,13 @@ export default function App({ Component, pageProps }: AppProps) {
                   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
                 }
               `}
-            />
+              />
 
-            <Component {...pageProps} />
-          </ChakraProvider>
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </ChakraProvider>
+              <Component {...pageProps} />
+            </ChakraProvider>
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </ChakraProvider>
+    </>
   );
 }
